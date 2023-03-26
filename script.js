00c923a9a40bb964699e8d45874de7c6b2c95d8b
bianca florescu <biancaflorescu97@yahoo.com>
2023-03-27 00:27:38 +0300

    created responsive carousel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar
 const navList = document.getElementById("nav-list");
 const navSelect = document.getElementById("nav-select");
 const navButton = document.getElementById("nav-button");
@@ -13,3 +14,78 @@ function navToggle() {
     navButton.style.display = "flex";
   }
 }
+
+// Carousel
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+const slideNumber = document.getElementById("slide-number");
+
+const prevButtonTablet = document.getElementById("slide-arrow-prev-tablet");
+const nextButtonTablet = document.getElementById("slide-arrow-next-tablet");
+
+const prevButtonMobile = document.getElementById("slide-arrow-prev-mobile");
+const nextButtonMobile = document.getElementById("slide-arrow-next-mobile");
+
+let numberOfPages = 1;
+
+const goToNextPage = (prev, next) => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+
+  slideNumber.innerText = `0${++numberOfPages}`;
+
+  prev.disabled = false;
+  prev.classList.remove("disabled");
+
+  if (numberOfPages === 4) {
+    next.disabled = true;
+    next.classList.add("disabled");
+  }
+};
+
+const goToPrevPage = (prev, next) => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+
+  if (numberOfPages > 1) {
+    slideNumber.innerText = `0${--numberOfPages}`;
+    prev.disabled = false;
+  }
+
+  if (numberOfPages < 4) {
+    next.disabled = false;
+    next.classList.remove("disabled");
+  }
+
+  if (numberOfPages === 1) {
+    prev.disabled = true;
+    prev.classList.add("disabled");
+  }
+};
+
+nextButton.addEventListener("click", () => {
+  goToNextPage(prevButton, nextButton);
+});
+
+prevButton.addEventListener("click", () => {
+  goToPrevPage(prevButton, nextButton);
+});
+
+nextButtonTablet.addEventListener("click", () => {
+  goToNextPage(prevButtonTablet, nextButtonTablet);
+});
+
+prevButtonTablet.addEventListener("click", () => {
+  goToPrevPage(prevButtonTablet, nextButtonTablet);
+});
+
+nextButtonMobile.addEventListener("click", () => {
+  goToNextPage(prevButtonMobile, nextButtonMobile);
+});
+
+prevButtonMobile.addEventListener("click", () => {
+  goToPrevPage(prevButtonMobile, nextButtonMobile);
+});
